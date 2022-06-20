d4d8129b8b53b7b7732d05af9eaa963ab1a5a206
ariffira <mac.fira@gmail.com>
2022-06-20 15:44:47 +0200

    express validation examples with react

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,176 @@
 import './App.css';
-import { useState } from "react";
-import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios"
+import { 
+  Button, 
+  Modal, 
+  ModalBody, 
+  ModalHeader, 
+  ModalFooter,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Alert
+} from 'reactstrap';
+import { useState } from 'react';
+
 function App() {
   // States
-  const [userInput, setUserInput] = useState()
-  const [movie, setMovie] = useState()
+  const [isOpen, setIsOpen] = useState(false)
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+  const [errors, setErrors] = useState([])
 
   // Handlers
-  const getData = (event)=>{
-    //console.log(event.target.value)
-    setUserInput(event.target.value)
+  const loginModal = ()=>{
+    setIsOpen(!isOpen)
   }
-  const searchMovie = ()=>{
-    axios.get("/movie/title/"+userInput)
-    .then(response => {
-      console.log(response.data)
-      setMovie(response.data)
+  const registrationModal = ()=>{
+    setIsOpen(!isOpen)
+  }
+
+  // send registration data to server
+  const getRegister = ()=>{
+    console.log(name, email,password)
+    // axios.method(url, data).then().catch()
+    axios.post('/auth/registration', {
+      name,email,password
+    }).then(response => {
+      console.log(response.data.errors)
+      setErrors(response.data.errors)
     })
   }
   
   return (
     <div className="App">
       <header className="App-header">
-        OMDB Movie Site
-        <input type="text" onChange={getData}/>
-        <button type="submit" onClick={searchMovie}>Search</button>
-        {
-          movie&&
-        <>
-        <h2>Movie Title: {movie.Title}</h2>
-        <h3>Plot: {movie.Plot}</h3>
-        <img src={movie.Poster} />
-        </>
-        }
+        <h3>Login Authentication Process Tutorial(Full-stack)</h3>
+
+        <Button color="success" onClick={loginModal}>LogIn</Button>
+        <br/>
+        <Button color="danger" onClick={registrationModal}>Registration</Button>
+        
+        {/* Modal for login*/}
+        <Modal isOpen = {isOpen}
+          toggle={loginModal}
+        >
+          <ModalHeader toggle={loginModal}>
+            Login
+          </ModalHeader>
+          <ModalBody>
+          <Form inline>
+          <FormGroup className="mb-2 me-sm-2 mb-sm-0">
+            <Label
+              className="me-sm-2"
+              for="exampleEmail"
+            >
+              Email
+            </Label>
+            <Input
+              id="exampleEmail"
+              name="email"
+              placeholder="something@idk.cool"
+              type="email"
+            />
+          </FormGroup>
+          <FormGroup className="mb-2 me-sm-2 mb-sm-0">
+            <Label
+              className="me-sm-2"
+              for="examplePassword"
+            >
+              Password
+            </Label>
+            <Input
+              id="examplePassword"
+              name="password"
+              placeholder="don't tell!"
+              type="password"
+            />
+          </FormGroup>
+        </Form>
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              color="primary"
+              onClick={loginModal}
+            >
+              Submit
+            </Button>
+            {' '}
+            <Button onClick={loginModal}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </Modal>
+
+        {/* Modal for registration*/}
+        <Modal isOpen = {isOpen} toggle={registrationModal}>
+          <ModalHeader toggle={registrationModal}>
+            Registration
+          </ModalHeader>
+          <ModalBody>
+          {
+          errors&&
+          errors.map((error, index)=>{
+           return(
+            <Alert color="danger" key={index}>
+            {error.msg}
+            </Alert>
+           )
+          })
+          }
+          <Form inline>
+          <FormGroup>
+            <Label for="exampleEmail">
+              Full-Name
+            </Label>
+            <Input type="text" onChange={(e)=> setName(e.target.value)} />
+          </FormGroup>
+          <FormGroup className="mb-2 me-sm-2 mb-sm-0">
+            <Label
+              className="me-sm-2"
+              for="exampleEmail"
+            >
+              Email
+            </Label>
+            <Input
+              id="exampleEmail"
+              placeholder="something@idk.cool"
+              type="email"
+              onChange={(e)=> setEmail(e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup className="mb-2 me-sm-2 mb-sm-0">
+            <Label
+              className="me-sm-2"
+              for="examplePassword"
+            >
+              Password
+            </Label>
+            <Input
+              id="examplePassword"
+              placeholder="don't tell!"
+              type="password"
+              onChange={(e)=> setPassword(e.target.value)}
+            />
+          </FormGroup>
+        </Form>
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              color="primary"
+              onClick={getRegister}
+            >
+              Submit
+            </Button>
+            {' '}
+            <Button onClick={registrationModal}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </Modal>
       </header>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -1,15 +1,35 @@
+// imports
 const express = require('express'); 
 const cors = require('cors');
 const app = express();
 require('dotenv').config()
-const PORT = 5000;
-// routes
-const movieRouter = require('./routes/movie.js')
-const seriesRouter = require('./routes/series.js')
+const PORT = 5000 || process.env.PORT;
+const {body, validationResult} = require('express-validator')
 
+// general settings and uses here
 app.use(cors());
-app.use('/movie', movieRouter);
-app.use('/series', seriesRouter);
+app.use(express.json())
+
+// backend api routes import here
+app.post('/auth/registration', 
+body('fullname').not().isEmpty()
+.withMessage('Full name should not be empty!'),
+body('email')
+.not().isEmpty()
+.withMessage('Its empty.Please give an email')
+.isEmail()
+.withMessage('This is not Valid email. Please write an valid email with @ and .'),
+body('pasword').not().isEmpty()
+.withMessage('Password should not be empty!')
+.isLength({min:4, max:10})
+.withMessage('Password must be 4-10 characters')
+.matches(/\d/)
+.withMessage('Password must contain a number')
+,(req, res)=>{
+    const errors = validationResult(req)
+    console.log(errors) 
+    res.json(errors)   
+})
 
 app.listen(PORT, (req, res)=> {
     console.log('Start...Server is running successfully on port ' + PORT)
